08da65847bc3dc25899de0a96de8bf24d29cd7ee
ivanhigueram <ivanhigueram@gmail.com>
2016-07-15 09:08:13 -0500

    Distances for black and indigenous territories.

--- raster_process_territories.R
+++ raster_process_territories.R
@@ -17,6 +17,15 @@ res_mask_buffers <- lapply(buffers_territories, function(x){
   mask(res[[1]], x)
 })
 
+#Identify cells from buffer and territory
+cells_territories <- lapply(territories, function(x){
+  cellFromPolygon(res[[1]], x)
+  })
+
+cells_territories_buffers <- lapply(buffers_territories, function(x){
+  cellFromPolygon(res[[1]], x)
+})
+
 #Create a list of individual polygons per territory. 
 polygon_to_list <- function(shape){
   shape@data$ID <- as.factor(row.names(shape@data))
@@ -56,7 +65,38 @@ system.time(distances_indigenous <- mapply(calculate_distances_parallel,
                                       points = list_polygons_p[[2]]))
 endCluster()
 
+###################################### EXTRACT #################################################
+#1. Extract distance as data frame per buffer (list element)
+list_dataframes_black <- pblapply(distances_black, as.data.frame, xy = T, na.rm = T)
+list_dataframes_indigenous <- pblapply(distances_indigenous, as.data.frame, xy = T, na.rm = T)
 
+#2. Extract row names (id cells)
+list_dataframes_black <- pblapply(list_dataframes_black, function(x){
+  x$ID <- row.names(x); x
+})
+
+list_dataframes_indigenous <- pblapply(list_dataframes_indigenous, function(x){
+  x$ID <- row.names(x); x
+})
+
+#3. Append all elements of the list 
+distance_dataframe_black <- do.call(rbind, list_dataframes_black)
+distance_dataframe_black$buffer_id <- rep(names(list_dataframes_black), sapply(list_dataframes_black, nrow)) #identify cells from buffers
+
+distance_dataframe_indigenous <- do.call(rbind, list_dataframes_indigenous)
+distance_dataframe_indigenous$buffer_id <- rep(names(list_dataframes_indigenous), sapply(list_dataframes_indigenous, nrow)) #identify cells from buffers
+
+#4. Identify treatment and remove NA's (read WARNING)
+distance_dataframe_black$treatment <- ifelse(distance_dataframe_black$ID %in% unlist(cells_territories[[1]]), 1, 0)
+distance_dataframe_indigenous$treatment <- ifelse(distance_dataframe_indigenous$ID %in% unlist(cells_territories[[2]]), 1, 0)
+
+list <- list(distance_dataframe_black, distance_dataframe_indigenous)
+names(list) <- c("black", "indigenous")
+
+#5. Export
+lapply(1:length(list), function(i){
+  write.csv(list[[i]], file = str_c("distance_dataframe_", names(list)[[i]], ".csv") , row.names = FALSE)
+})
 
 
 
