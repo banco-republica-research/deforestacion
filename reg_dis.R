29ea7bf5b0bc4c571a1a20bf7ce66d3c396fc5c6
ivanhigueram <ivanhigueram@gmail.com>
2016-07-23 14:08:08 -0500

    RD Graphs and RD Estimators (using robust-corrected bw).

--- reg_dis.R
+++ reg_dis.R
@@ -16,8 +16,8 @@ setwd("~/Dropbox/BANREP/Deforestacion/Datos/Dataframes")
 defo <- read.csv("dataframe_deforestacion.csv")
 list_files <- list.files()
 rds_2000 <- list_files[str_detect(list_files, "dist_2000")][c(1:3)] %>%
-  lapply(., readRDS) %>%
-  lapply(., data.frame)
+  lapply(readRDS) %>%
+  lapply(data.frame)
 
 #Aggregate deforestation (2001 - 2012)
 defo$loss_sum <- rowSums(defo[, c(4:length(names(defo)))])
@@ -40,21 +40,33 @@ stargazer(naive_reg)
 #Regression discontinuity 
 
 #All parks RD estimator
-p <- rdrobust(
-  y = defo_dist$loss_sum,
-  x = defo_dist$dist_disc
-)
+rd_robust_parks <- lapply(defo_dist, function(park){
+  rdrobust(
+    y = park$loss_sum,
+    x = park$dist_disc,
+    cluster = park$ID
+  )
+  })
+
+
+rdd_parks <- rdd::RDestimate(loss_sum ~ I(dist_disc/1000),
+                              data = defo_dist[[1]],
+                              cluster = defo_dist[[1]]$ID, 
+                              cutpoint = 0,
+                              frame = T
+) 
+
 
 setwd("~/Dropbox/BANREP/Deforestacion/Results/RD/Graphs/")
 mapply(function(x, type){
   pdf(str_c("RD_", type, ".pdf"), height=6, width=12)
   rdplot(
-    y = x$loss_sum,
+    y = (x$loss_sum) * 100,
     x = x$dist_disc,
-    y.lim = c(0, 0.2),
-    title = str_c("Regresion discontinuity for", type, sep = " "),
-    x.label = "Distance to national park frontier",
-    y.label = "Deforestation (%)")
+    y.lim = c(0, 10),
+    title = str_c("Regression discontinuity for", type, sep = " "),
+    x.label = "Distance to national park frontier (meters)",
+    y.label = "Deforestation (Ha x km2)")
   dev.off()
 }, x = defo_dist, type = c("all parks", "national parks", "regional parks"))
 
