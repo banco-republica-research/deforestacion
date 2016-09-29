cfbd2eb997f720c8c1da6e0707367f68fb8326fc
leonardobonilla <leonardobonilla84@gmail.com>
2016-09-29 16:54:22 -0500

    cambios menores

--- Data/reg_panel.R
+++ Data/reg_panel.R
@@ -28,23 +28,29 @@ defo <- defo[, (c("loss_year_brick_1km.1","loss_year_brick_1km.14")) := NULL]
 clumps <- fread("Datos/Clumps/clump_id_dataframe_2000.csv")
 clumps <- subset(clumps,,c("ID","clumps"))
 geo <- fread(paste0(data,"geographic_covariates.csv"))
-geo <- subset(geo,,c( "ID", "altura_tile_30arc", "slope", "roughness", "tri"))
+geo <- subset(geo,,c( "ID", "altura_tile_30arc", "slope", "roughness", "tri","prec","sq_1km.1","roads"))
+tree <- fread(paste0(data,"treecover_2000.csv"))
+tree <- subset(tree,,c( "ID", "treecover_agg"))
 mun <- fread(paste0(data,"colombia_municipios_code_r.csv"))
 mun <- subset(mun,,c("ID","layer"))
 
 x <-  merge(clumps,geo, by = "ID", all = TRUE)
+x <-  merge(x,tree, by = "ID", all = TRUE)
 x <-  merge(x,mun, by = "ID", all = TRUE)
 
-conflict <- read.dta("Datos/Conflicto/conflicto_pre2000.dta")
-x <-  merge(x,conflict, by.x = "layer", by.y = "codmun", all = TRUE)
+# conflict <- read.dta("Datos/Conflicto/conflicto_pre2000.dta")
+# x <-  merge(x,conflict, by.x = "layer", by.y = "codmun", all = TRUE)
+write.dta(x,paste0(data,"covariates.dta"))
+
+x_hete <- subset(x,,c("ID","clumps","roads"))
+
 
 ############################
 # Merge distances by type of area to defo and X
 
-# areas <- c("all","national","regional", "terr1", "terr2")
-areas <- c("terr1", "terr2")
+areas <- c("all","national","regional", "terr1", "terr2")
 
-for(d in c(1:1)) { 
+for(d in c(1:2)) { 
   print(paste0("distance ",d))
   
   for(a in areas) {
@@ -54,7 +60,7 @@ for(d in c(1:1)) {
     defo_temp <- melt(defo_temp, id.vars = "ID", measure = patterns("^loss_year_brick_1km."), variable.name = "year", value.name = "defo")
     defo_temp$year <- as.numeric(substr(defo_temp$year, 21,23)) + 1999
     defo_temp <- merge(dist_temp, defo_temp,by = c("ID", "year"))
-    defo_dist <- merge(defo_temp, x, by ="ID", all.x = TRUE)
+    defo_dist <- merge(defo_temp, x_hete, by ="ID", all.x = TRUE)
     defo_dist$clumps[is.na(defo_dist$clumps)] <- 0
 
     # Export to stata
@@ -77,7 +83,7 @@ for(d in c(1:2)) {
       defo_temp <- melt(defo_temp, id.vars = "ID", measure = patterns("^loss_year_brick_1km."), variable.name = "year", value.name = "defo")
       defo_temp$year <- as.numeric(substr(defo_temp$year, 21,23)) + 1999
       defo_temp <- merge(dist_temp, defo_temp,by = c("ID", "year"))
-      defo_dist <- merge(defo_temp, x, by ="ID", all.x = TRUE)
+      defo_dist <- merge(defo_temp, x_hete, by ="ID", all.x = TRUE)
       defo_dist$clumps[is.na(defo_dist$clumps)] <- 0
       
       # Export to stata
