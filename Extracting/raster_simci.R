7257e65c9c3cc67b5b86ad4efdc49cbf8c38cb95
ivanhigueram <ivanhigueram@gmail.com>
2017-10-13 08:00:38 -0500

    Data extraction from SIMCI rasters
    This commit includes R scripts to read Python processed rasters and
    extract their features. To do this, we read the data and resample it in
    order to have comparable observations across datasets (i.e.
    deforestation, nightlights, and covariates). In the raster_simci.R we
    leave a piece of R code as an example of how to run the rasterization in
    R instead of Python.
    
    TODO:
    
     - Set up folders with global environment variables

--- Extracting/raster_simci.R
+++ Extracting/raster_simci.R
@@ -21,18 +21,57 @@ list_raster <- list.files(paste0(paste0(data, "NOAA2/TIFF/")), full.names = TRUE
 rasters_extent <- extent(list_raster[[1]]) #We need to put all rasters into the same extent (all have the same resolution)
 rasters_lights <- processing_rasters(list_raster, rasters_extent, colombia_municipios)
 writeRaster(rasters_lights[[1]], paste0(data, "NOAA2/", "light_brick_colombia.tif"), overwrite = TRUE)
-  
-# "Coca_01_16_grillas1k", 
-# "EVOA_MTPL_1k_18N"
 
+  
+####################################################################
+####################### RASTERIZE SHAPEFILES #######################
+####################################################################
+
+# THIS PROCESS WAS MADE IN PYTHON FOR EFFICIENCY REASONS. PYTHON IS
+# FASTER TO OPEN GRID POLYGON DATA AND MADE A COMPARABLE JOB TO R 
+# WHEN CREATING RASTERS. THIS CODE CAN BE USE IN CASE YOU WANT TO HAVE
+# ALL THE DATA CREATION PIPELINE IN R.
+
+# Open shapefiles and project to a common reference
 simci_data <- lapply(c("Coca_01_16_grillas1k", "EVOA_MTPL_1k_18N"), function(x){
-  st_read(dsn = paste0(data, "SIMCI"), layer = x) %>%
-    as(., "Spatial") %>%
+  readOGR(dsn = paste0(data, "SIMCI"), layer = x) %>%
     spTransform(CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")) 
 })
 
-r_ <- raster::rasterize(x = r, 
-                        y = rasters_lights,
-                        field = names(r)
+# Example of rasterize
+r_ <- raster::rasterize(x = simci_data[[1]], 
+                        y = rasters_lights[[1]],
+                        field = "COCA_C_11",
+                        filename = "coca_COCA_C_11.tif",
+                        overwrite = T
                         )
+######################################################################
+
+
+res <- brick(paste0(data, "HansenProcessed", "/", "loss_year_brick_1km.tif"))
+treecover <- brick(paste0(data, "HansenProcessed", "/", "treecover_1km.tif"))
+
+
+#Open rasterized rasters from Python (raster_simci.py)
+file_names <- c("coca", "illegal_mining")
+
+simci_rasters <- lapply(file_names, function(x){
+  #Open and process rasters (crop and set extent)
+  proc_raster <- list.files(paste0(data, "SIMCI"), full.names = TRUE) %>%
+    .[str_detect(., x)] %>%
+    lapply(raster) %>%
+    processing_rasters(., extent(res), colombia_municipios)
   
+  #Resample to deforestation data (make all pixels comparable - last resort after crop and extent)
+  raster_resampled <- resample(proc_raster, res, filename = paste0(data, "SIMCI", "/", x, "_1km_brick.tif"),
+                               format = "GTiff",
+                               options = "INTERLEAVE=BAND", 
+                               progress = "text", overwrite = T)
+  
+  #Extract data
+  raster_plain_csv <- as.data.frame(raster_resampled, xy = TRUE, na.rm = TRUE)
+  raster_plain_csv$ID <- row.names(raster_plain_csv)
+  write.csv(raster_plain_csv, paste0(data, "Dataframes", "/", x, "simci.csv"))
+})
+
+identical(coordinates(simci_rasters[[1]]), coordinates(res[[1]]))
--- Extracting/src/rasterize_variables.py
+++ Extracting/src/rasterize_variables.py
@@ -7,7 +7,9 @@ def rasterize(name, shape, variable, template_meta):
     '''
     This function takes a shapefile data and rasterize its data to create
     new arrays with template_meta specifications. The function will print out a
-    new GeoTIFF under the name defined in name variable.
+    new GeoTIFF under the name defined in name variable. If the all_touched
+    option is uncommented, the function will update the values of the raster
+    using the painting algorithm.
     '''
     file_name = str(name + '_' + variable + '.tif')
     with rasterio.open(os.path.join(os.environ['DATA_FOLDER'], 'SIMCI' ,file_name), 'w', **template_meta) as out:
@@ -18,7 +20,8 @@ def rasterize(name, shape, variable, template_meta):
                                                           shape[variable]))
             burned = features.rasterize(shapes = shapes, fill = 0, out = out_arr,
                                                        transform = out.transform,
-                                                       all_touched = True)
+                                                       #all_touched = True 
+                                       )
             out.write_band(1, burned)
 
         else:
