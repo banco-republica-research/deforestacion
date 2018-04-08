41b0ae8b190fc93bd55051af36e8a55cfffdc4b7
ivanhigueram <ivanhigueram@gmail.com>
2018-04-08 18:18:07 -0500

    New covariates: clumps for 5km and 10km
    To solve estimation problems in areas with low population/light, we
    create new covariates: clumps within a 5km and 10 km influence zone. The
    past code used the clump raster function to identify cells with values
    inside the raster. We created  a shape file geometry from the clump, and
    apply a buffer to design this influence zones.
    
    Cells are identified inside the "res[[1]]" deforestation template raster
    and saved as csv as [1,0] dummy variable

--- extracting/raster_covariates.R
+++ extracting/raster_covariates.R
@@ -12,42 +12,58 @@ library(dplyr)
 library(stringr)
 
 # Load functions in R
-setwd("~/GitHub/deforestacion/")
-source("R/process_rasters.R") 
+setwd("~/deforestacion/")
+source("R/process_rasters.R")
+source("R/clean_polygons.R")
+source("R/calculate_distances.R")
+source("cleaning/colombia_continental.R")
 
 # Set directories
-data <- "Deforestacion/Datos/"
-setwd("~/Dropbox/BANREP/")
+setwd(Sys.getenv("DATA_FOLDER"))
 
-#Get deforestation raster for reference 
-res <- brick(paste0(data, "HansenProcessed/1.4/loss_year_brick_1km.tif"))
-
-# Load Colombia shapefile to mask/crop rasters
-colombia_municipios <- 
-  readOGR(dsn = paste0(data, "Geografia"), layer="Municipios") %>%
-  spTransform(CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-
-
+#Get deforestation raster for reference: deforestation
+res <- brick("HansenProcessed/1.4/loss_year_brick_1km.tif")
 
 ###############################################################################
 ######################## NIGHT LIGHTS CLUMPS (FROM NOAA) ######################
 ###############################################################################
 
 # Open nightlight .tif files as a raster and create a multiband file
-list_raster <- list.files(paste0(paste0(data, "NOAA2/TIFF/")), full.names = TRUE) %>%
+list_raster <- list.files(paste0("NOAA2/TIFF/"), full.names = TRUE) %>%
   lapply(raster)
 rasters_extent <- extent(list_raster[[1]]) #We need to put all rasters into the same extent (all have the same resolution)
-rasters_lights <- processing_rasters(list_raster, rasters_extent, colombia_municipios)
-writeRaster(rasters_lights[[1]], paste0(data, "NOAA2/", "light_brick_colombia.tif"), overwrite = TRUE)
+rasters_lights <- processing_rasters(list_raster, rasters_extent, colombia_municipios[[1]])
+writeRaster(rasters_lights[[1]], paste0("NOAA2/", "light_brick_colombia.tif"), overwrite = TRUE)
 
 #Clumps to identify cities (queen) for 2005
 clump_lights <- clump(rasters_lights[[13]], directions = 8) %>%
   resample(., res[[1]])
 clump_lights[clump_lights > 1] <- 1
 
+p1 <- rasterToPolygons(clump_lights, dissolve = T)
+
+#Set a bigger zone of light influence (buffer: 5 and 10 k's)
+
+p1_buffer_5km <-  rasterToPolygons(clump_lights, dissolve = T) %>%
+  spTransform(CRS=CRS("+init=epsg:3857")) %>%
+  gBuffer(width = 5000) %>%
+  spTransform(CRS = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+
+p1_buffer_10km <-  rasterToPolygons(clump_lights, dissolve = T) %>%
+  spTransform(CRS=CRS("+init=epsg:3857")) %>%
+  gBuffer(width = 10000) %>%
+  spTransform(CRS = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+
+clumps_buffer_5k <- cellFromPolygon(res[[1]], p1_buffer_5km)
+clumps_buffer_10k <- cellFromPolygon(res[[1]], p1_buffer_10km)
+
+
+
 #Clumps to polygons
 p1 <- rasterToPolygons(clump_lights, dissolve = T)
-writeOGR(p1, paste0(data, "Clumps", "/", "polygon_clump_layer_2000.shp"), 
+writeOGR(p1, paste0("Clumps", "/", "polygon_clump_layer_2000.shp"), 
          layer = "clumps", 
          driver = "ESRI Shapefile", 
          overwrite_layer = T)
@@ -58,9 +74,9 @@ writeOGR(p1, paste0(data, "Clumps", "/", "polygon_clump_layer_2000.shp"),
 ###############################################################################
 
 #Process data
-elevation <- raster(paste0(data, "Rasters", "/", "altura_tile_30arc.tif")) %>%
-  crop(colombia_municipios) %>%
-  mask(colombia_municipios) %>%
+elevation <- raster(paste0("Rasters", "/", "altura_tile_30arc.tif")) %>%
+  crop(colombia_municipios[[1]]) %>%
+  mask(colombia_municipios[[1]]) %>%
   resample(., res[[1]])
 
 # Calcuate slope and aspect
@@ -75,14 +91,14 @@ tri <- terrain(elevation, opt = "TRI")
 ###############################################################################
 
 #Get climate 
-list_files <- list.files(paste0(data, "Rasters", "/", "wc2"), full.names = T) %>%
+list_files <- list.files(paste0("Rasters", "/", "wc2"), full.names = T) %>%
   str_detect("prec")
 
 prec <- lapply(list.files()[list_files], raster) %>%
   brick() %>%
-  lapply(crop, colombia_municipios) %>%
-  lapply(mask, colombia_municipios) %>%
-  resample(res[[1]], filename = paste0(data, "Rasters", "/", "prec_1km.tif"),
+  lapply(crop, colombia_municipios[[1]]) %>%
+  lapply(mask, colombia_municipios[[1]]) %>%
+  resample(res[[1]], filename = paste0("Rasters", "/", "prec_1km.tif"),
            format = "GTiff",
            options = "INTERLEAVE=BAND", 
            progress = "text", overwrite = T)
@@ -92,13 +108,13 @@ prec <- lapply(list.files()[list_files], raster) %>%
 ########################### SOIL QUALITY (FROM FAO) ###########################
 ###############################################################################
 
-list_files <- list.files(paste0(data, "Rasters", "/", "soil_quality"), full.names = T)
+list_files <- list.files(paste0("Rasters", "/", "soil_quality"), full.names = T)
 
 sq <- lapply(list_files, raster) %>%
   brick() %>%
   crop(colombia_municipios) %>%
   mask(colombia_municipios) %>%
-  resample(res[[1]], filename = paste0(data, "Rasters", "/", "soil_quality", "/", "sq_1km.tif"),
+  resample(res[[1]], filename = paste0("Rasters", "/", "soil_quality", "/", "sq_1km.tif"),
            format = "GTiff",
            options = "INTERLEAVE=BAND", 
            progress = "text", overwrite = T)
@@ -109,7 +125,7 @@ sq <- lapply(list_files, raster) %>%
 ###############################################################################
 
 #Roads
-roads <- readOGR(dsn = paste0(data, "Roads"), layer=paste0(data, "VIAS")) %>%
+roads <- readOGR(dsn = "Roads", layer="VIAS") %>%
   # spTransform(CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")) to mercator
   spTransform(CRS=CRS("+init=epsg:3857")) 
 
@@ -118,7 +134,7 @@ buffer_roads <- gBuffer(roads, 5000, byid = TRUE, id = row.names(roads)) %>%
 cells_roads <- cellFromPolygon(res[[1]], buffer_roads)
 
 cells_roads <- unlist(cells_roads)
-write.csv(cells_roads, paste0(data, "Dataframes", "/", "roads.csv"))
+write.csv(cells_roads, paste0("Dataframes", "/", "roads.csv"))
 
 
 #Extract data to data.frame
@@ -131,11 +147,13 @@ dataframes_extract <- list(elevation, slope, roughness, tri, clump_lights, prec,
 #Merge
 merge_rasters_dataframes <- Reduce(function(...) merge(..., by="ID", all = T), dataframes_extract) %>%
   mutate(roads = ifelse(ID %in% unlist(cells_roads), 1, 0)) %>%
+  mutate(clumps_5k = ifelse(ID %in% unlist(clumps_buffer_5k), 1 ,0)) %>%
+  mutate(clumps_10k = ifelse(ID %in% unlist(clumps_buffer_10k), 1, 0)) %>%
   mutate(clumps_1 = ifelse(is.na(clumps), 0, 1)) %>%
   mutate(prec = select(., starts_with("prec")) %>% rowMeans(na.rm = TRUE))
 
 #Export .csv with clumps and ID
-write.csv(merge_rasters_dataframes, paste0(data, "Dataframes", "/", "geographic_covariates.csv"))
+write.csv(merge_rasters_dataframes, paste0("Dataframes", "/", "geographic_covariates_new.csv"))
 
 
 
