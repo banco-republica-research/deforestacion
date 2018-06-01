561ef7580195379bc316f021edab9ec55323d27e
ivanhigueram <ivanhigueram@gmail.com>
2018-05-31 22:16:11 -0500

    New df with buffer descriptive stats plus re-order
    Included to the set of permutation test, this new code generates a new
    df with descriptive stats for defined vars and a numerical buffer. The
    function is called and needs a list of df with covariates and defined
    list of covariates, buffer distance, distance variable, and a named
    vector to distinguish the df's

--- R/rd_functions.R
+++ R/rd_functions.R
@@ -17,12 +17,12 @@ rd_robust <- function(df, x, y, nn, bw = NULL, covs_matrix = NULL){
 
 
 ###############################################################################
-## 			PERMUTATION TESTS: CANAY & CAMAT (2017) 	     ##
+## 		            	PERMUTATION TESTS: CANAY & CAMAT (2017) 	               ##
 ## This function calculates the Canay and Camat test to check violations to  ##
 ## the RD assumptions. The function is a simple wrapper to the RATest pkg    ##
 ## and will simply allow to apply the function to a list of dataframes       ##
 ## returning a concatenated dataframe with the statistics plus a id colum    ##
-## list_df = data.frame list                    			     ##
+## list_df = data.frame list                    			                       ##
 ## names = names to add as id for each dataframe                             ##
 ## covs = vector of named covariates                                         ##
 ## c = cutoff (set as 0)                                                     ##
@@ -51,10 +51,64 @@ perm_test_list <- function(list_df, names, covs, z, c = 0, ...){
         # Concatenate data.frames and spit out a dataframe with df ids
 	names_rep <- rep(names, length(covs)+1) %>% 
 		factor(., levels=names) %>% 
-		sort(x) %>% as.character()
+		sort(.) %>% as.character(.)
 
         perm_df_id <- perm_df %>% ldply %>% mutate(df_id = names_rep)
         return(perm_df_id)
       }
-    }
+}
+
+
+
+
+descriptive_stats_buffer <- function(list_df, 
+                                     dist_var, 
+                                     buffer = 5, 
+                                     vars, 
+                                     treatment = FALSE,
+                                     names){
+  if(is.null(vars)){
+    stop('Put some variables to calculate stuff')
+  }
+  if(treatment==FALSE){
+    warning('No specified buffer. A 5 km buffer will be used as default')
+    buffer = 5
+    
+    filter_list <- lapply(list_df, function(x){
+      print(buffer)
+      
+      df_sum_stats <- x %>%  
+        filter(abs(UQ(sym(dist_var))) <= buffer) %>%
+        dplyr::select(vars) %>%
+        mutate_all(., funs(as.numeric)) %>%
+        set_colnames(str_replace(names(.), '_', '')) %>%
+        summarize_all(., funs(
+                            min = min(., na.rm=T),
+                            max = max(., na.rm=T),
+                            mean = mean(., na.rm=T),
+                            sd = sd(., na.rm=T))
+                       )
+      #return(df_sum_stats)
+      
+       df_shape <- df_sum_stats %>% 
+         gather(stat, val) %>% 
+         separate(stat, into = c('var', 'stat'), sep = '_') %>% 
+         spread(stat, val) %>%
+         dplyr::select(var, mean, sd, min, max)
+      
+       return(df_shape)
+    })
+  }
+  
+  named_list <- mapply(function(df, name){
+    df_named <- df %>%
+      mutate(area = name)
+  }, df = filter_list, names, SIMPLIFY = F)
+  
+  summary_df <- named_list %>% ldply() %>%
+    select(area, everything())
+  
+  return(summary_df)
+}
+
 
---
+++ modeling/merge_datasets.R
@@ -0,0 +1,79 @@
+##############################################################################################
+##############################################################################################
+###                MERGE DATA SETS AND CREATE DATAFRAMES FOR REGRESSION                    ###
+###     THIS CODE WILL READ THE DISTANCE AND DEFORESTATION DATA TO RUN RD MODELS           ###
+###     HERE WE WILL RUN THE MODELS FOR BOTH STRATEGIES (1: REMOVE CONTINENTAL BORDERS AND ###
+###             2. REMOVE ALL COTREATMENT BORDERS AND USE EFFECTIVE BORDERS).              ###
+##############################################################################################
+##############################################################################################
+
+library(plyr)
+library(dplyr)
+library(data.table)
+library(rdrobust)
+library(rdd)
+library(stringr)
+library(stargazer)
+library(foreign)
+library(ggplot2)
+library(magrittr)
+library(foreign)
+library(stringr)
+
+
+# Set directories
+setwd(Sys.getenv("DATA_FOLDER"))
+
+########################################## STRATEGY 2: EFFECTIVE BORDERS ###############################################
+
+#Import datasets (covariates)
+defo <- read.csv(paste0("Dataframes", "/","dataframe_deforestacion.csv")) %>% dplyr::select(-X)
+cov <- read.csv(paste0("Dataframes", "/","geographic_covariates_new.csv"))
+treecover <- read.csv(paste0("Dataframes", "/", "treecover_2000.csv")) %>% dplyr::select(ID, treecover_agg)
+simci_coca <- read.csv(paste0( "Dataframes", "/", "coca_simci_extract.csv")) %>% dplyr::select(contains("coca"), ID)
+simci_mining <- read.csv(paste0("Dataframes", "/", "illegal_mining_simci_extract.csv")) %>% dplyr::select(contains("EVOA"), ID)
+
+
+list_files <- list.files(paste0("Dataframes/", "Estrategia 2"), full.names = TRUE)
+rds_2000 <- list_files[str_detect(list_files, "dist_2000")][c(1:3)] %>%
+  lapply(readRDS) %>%
+  lapply(data.frame)
+
+list_files <- list.files(paste0("Dataframes/", "Estrategia 2"), full.names = TRUE)
+territories_2000 <- list_files[str_detect(list_files, "_2000")] %>%
+  .[str_detect(., "terr")] %>%
+  lapply(readRDS) %>%
+  lapply(data.frame)
+
+
+#Aggregate deforestation (2001 - 2012) and Coca crops (2001 - 2012)
+defo$loss_sum <- rowSums(defo[, c(5:length(names(defo)) - 1 )])
+loss_sum <- dplyr::select(defo, c(ID, loss_sum)) %>% mutate(loss_sum = loss_sum / 16)
+simci_coca$coca_agg <- rowMeans(simci_coca[, c(1:16)], na.rm = T)
+
+
+
+#Merge data
+defo_dist <- lapply(rds_2000, function(x){
+  merge(loss_sum, x, by.x = "ID", by.y = "ID") %>%
+    mutate(., loss_sum = loss_sum * 100) %>%
+    mutate(., dist_disc = ifelse(treatment == 1, 1, -1) * layer) %>%
+    mutate(., dist_disc = dist_disc / 1000) %>%
+    # mutate(., buffer_id = as.character(buffer_id)) %>% mutate(., buffer_id = as.factor(buffer_id))
+    merge(., cov, by = "ID", all.x = T) %>%
+    merge(., treecover, by = "ID") %>%
+    merge(., simci_coca, by = "ID", all.x = T) %>%
+    merge(., simci_mining, by = "ID", all.x = T)
+})
+
+defo_dist_terr <- lapply(territories_2000, function(x){
+  merge(loss_sum, x, by.x = "ID", by.y = "ID") %>%
+    mutate(., loss_sum = loss_sum * 100) %>%
+    mutate(., dist_disc = ifelse(treatment == 1, 1, -1) * dist) %>%
+    mutate(., dist_disc = dist_disc / 1000) %>%
+    # mutate(., buffer_id = as.character(buffer_id)) %>% mutate(., buffer_id = as.factor(buffer_id))
+    merge(., cov, by = "ID", all.x = T) %>%
+    merge(., treecover, by = "ID") %>%
+    merge(., simci_coca, by = "ID", all.x = T) %>%
+    merge(., simci_mining, by = "ID", all.x = T)
+})
